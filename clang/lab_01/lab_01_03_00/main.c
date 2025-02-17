d8f55533ded852bbfb7b845fc39d5626c5af1cdb
Dayz221 <rutuchenal37@gmail.com>
2025-02-17 10:12:21 +0300

    some changes in clang lab_01

--- clang/lab_01/lab_01_03_00/main.c
+++ clang/lab_01/lab_01_03_00/main.c
@@ -10,20 +10,20 @@ int main()
     double t; // Обхват груди
     double h; // Высота
 
-    printf("Enter m, t and h: ");
-    scanf("%lf%lf%lf", &m, &t, &h);
+    printf("Enter h, t and m: ");
+    scanf("%lf%lf%lf", &h, &t, &m);
 
     // Необходимые вычисления
     double m_normal;
     double bmi;
 
     m_normal = h * t / 240.;
-    h *= METERS_IN_CENTIMETER; // Перевод из сантиметров в метры
-    bmi = m / (h * h);
+    // h *= METERS_IN_CENTIMETER; // Перевод из сантиметров в метры
+    bmi = m / (h * h) / (METERS_IN_CENTIMETER * METERS_IN_CENTIMETER);
 
     // Вывод результата
-    printf("M normal: %.6f\n", m_normal);
-    printf("BMI: %.6f\n", bmi);
+    printf("M normal: %.6lf\n", m_normal);
+    printf("BMI: %.6lf\n", bmi);
 
     return 0;
 }
